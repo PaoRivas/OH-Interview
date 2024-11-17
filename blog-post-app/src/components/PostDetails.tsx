693b8402cbd8fc2b8142fb106536064138e866ab
Paola Rivas <paoa.rivasm@gmail.com>
2024-11-17 12:33:24 -0400

    Implement redux to show different posts

--- blog-post-app/src/components/PostDetails.tsx
+++ blog-post-app/src/components/PostDetails.tsx
@@ -1,16 +1,24 @@
+import { RootState } from '@/app/store';
 import { Box, Heading, Text } from '@chakra-ui/react';
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+
+export const formatDate = (date: string): string => {
+    return new Date(date).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })
+}
 
 const PostDetails = () => {
-
+    const { id } = useParams<{ id: string }>();
+    const post = useSelector((state: RootState) =>
+        state.posts.find((p) => p.id === Number(id))
+    );
+    if (!post) return <Text>Post not found</Text>;
     return (
         <Box p={4}>
-            <Heading mt={4}>Title</Heading>
-            <Text mt={4} fontWeight="bold">Author: Paola</Text>
-            <Text>Date: 12/08/24</Text>
-            <Text mt={3} w={{ base: "100%", lg: "60%" }} mx="auto">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-
-                The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</Text>
-
+            <Heading mt={4}>{post.title}</Heading>
+            <Text mt={4} fontWeight="bold">Author: {post.author}</Text>
+            <Text>{formatDate(post.date)}</Text>
+            <Text mt={3} w={{ base: "100%", lg: "60%" }} mx="auto">{post.content}</Text>
         </Box>
     );
 };
--- blog-post-app/src/components/PostList.tsx
+++ blog-post-app/src/components/PostList.tsx
@@ -1,22 +1,21 @@
+import { RootState } from "@/app/store";
 import { Box, Heading, Stack, Text } from "@chakra-ui/react";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import type { Post } from "@/features/postSlice";
 
 const PostList = () => {
+    const posts: Post[] = useSelector((state: RootState) => state.posts);
 
     return (
         <Stack p={4} textAlign="start" w={{ base: "100%", lg: "60%" }} mx="auto">
-            <Link to={"/post/1"}>
-                <Box p={3} borderWidth={1} borderRadius="md" color="black">
-                    <Heading size="xl" letterSpacing="tight">Blog Post Title</Heading>
-                    <Text lineClamp="2" mt={2} fontWeight="normal" color="fg.muted">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. </Text>
-                </Box>
-            </Link>
-            <Link to={"/post/2"}>
-                <Box p={3} borderWidth={1} borderRadius="md" color="black">
-                    <Heading size="xl" letterSpacing="tight">Blog Post Title</Heading>
-                    <Text lineClamp="2" mt={2} fontWeight="normal" color="fg.muted">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. </Text>
-                </Box>
-            </Link>
+            {posts.map((post) => (
+                <Link key={post.id} to={`/post/${post.id}`}>
+                    <Box p={3} borderWidth={1} borderRadius="md" color="black">
+                        <Heading size="xl" letterSpacing="tight">{post.title}</Heading>
+                        <Text lineClamp="2" mt={2} fontWeight="normal" color="fg.muted">{post.description}</Text>
+                    </Box>
+                </Link>))}
         </Stack>
     );
 };
